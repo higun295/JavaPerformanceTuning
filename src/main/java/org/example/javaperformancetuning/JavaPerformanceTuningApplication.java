15cd15db6817b1677d749e81f7fe151028dbe78b
HEEKWON <higun295@gmail.com>
2024-08-19 22:36:36 +0900

    first commit

---
+++ src/main/java/org/example/javaperformancetuning/JavaPerformanceTuningApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaperformancetuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPerformanceTuningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPerformanceTuningApplication.class, args);
+    }
+
+}
