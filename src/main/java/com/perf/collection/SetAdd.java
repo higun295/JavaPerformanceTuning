022e2e9d80e5988e04af97113863231322e3737a
HEEKWON <higun295@gmail.com>
2024-08-22 00:59:31 +0900

    add new test

--- src/main/java/com/perf/collection/SetAdd.java
+++ src/main/java/com/perf/collection/SetAdd.java
@@ -1,4 +1,47 @@
 package com.perf.collection;
 
+import org.openjdk.jmh.annotations.*;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@BenchmarkMode({ Mode.AverageTime })
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class SetAdd {
+    int LOOP_COUNT = 100000;
+    Set<String> set;
+    String data = "abcdefghijklmnopqrstuvwxyz";
+
+    @Benchmark
+    public void addHashSet() {
+        set = new HashSet<String>();
+        for(int loop = 0; loop < LOOP_COUNT; loop++) {
+            set.add(data + loop);
+        }
+    }
+    @Benchmark
+    public void addTreeSet() {
+        set = new TreeSet<String>();
+        for(int loop = 0; loop < LOOP_COUNT; loop++) {
+            set.add(data + loop);
+        }
+    }
+    @Benchmark
+    public void addLinkedHashSet() {
+        set = new LinkedHashSet<String>();
+        for(int loop = 0; loop < LOOP_COUNT; loop++) {
+            set.add(data + loop);
+        }
+    }
+    @Benchmark
+    public void addHashSetWithInitialSize() {
+        set = new HashSet<String>(LOOP_COUNT);
+        for(int loop = 0; loop < LOOP_COUNT; loop++) {
+            set.add(data + loop);
+        }
+    }
 }
